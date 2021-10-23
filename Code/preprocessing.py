443860eafb43f3580291c76145af65642f4339b3
SLAM-CROC <siqizhao98@163.com>
2021-10-23 17:42:43 -0600

    update preprocessing code

---
+++ Code/preprocessing.py
@@ -0,0 +1,140 @@
+import re
+import tensorflow as tf
+import csv
+import numpy as np
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+
+# Load csv file and remove blank data
+def load_csv(filename):
+    features = []
+    labels = []
+    count = 1
+    blank_data_number_list = []
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        for line in reader:
+            # print(line[1:3])
+            if line[1] == 'title':
+                count += 1
+                continue
+            elif line[1] == ' ' or line[2] == ' ':
+                blank_data_number_list.append(count)
+                count += 1
+                continue
+            features.append(line[1:3])
+            labels.append(line[3])
+            count += 1
+    print("Loaded csv file, and there are " + str(len(blank_data_number_list)) + " blank text have been removed "
+                                                                                 "from dataset\n" + str(len(features)) +
+          " data points in total")
+    return features, labels
+
+
+def split_data(features, labels, test_proportion):
+    index = int(len(features) * (1 - test_proportion))
+    train_x, train_y = np.array(features[:index]), np.array(labels[:index])
+    test_x, test_y = np.array(features[index:]), np.array(labels[index:])
+    return (train_x, train_y), (test_x, test_y)
+
+
+def remove_url(features):
+    count = 0
+    for i in features:
+        if 'http://' in i[1] or 'https://' in i[1]:
+            count += 1
+            i[1] = re.sub(r'http\S+', ' ', i[1])
+    print("There are "+str(count)+" url have been removed from text")
+
+
+def remove_newline(features):
+    for i in features:
+        i[1] = i[1].replace('\n', ' ').replace('\r', ' ').replace('\n\n', ' ')
+    print("Newline symbols have been removed from text")
+
+
+def remove_number(features):
+    for i in features:
+        i[1] = re.sub(r'\d+', ' ', i[1])
+    print("Numbers have been removed from text")
+
+
+def remove_punctuation(features):
+    for i in features:
+        i[1] = re.sub('[^a-zA-Z]', ' ', i[1])
+    print("Punctuations have been removed from text")
+
+
+def convert_into_lowercase(features):
+    for i in features:
+        i[1] = i[1].lower()
+    print("All text have been converted into lowercase")
+
+
+def tokenization(features):
+    for i in features:
+        i[1] = word_tokenize(i[1])
+    print("Preformed tokenization")
+
+
+def remove_stopwords(features):
+    stop_words = set(stopwords.words('english'))
+    for i in features:
+        i[1] = [words for words in i[1] if not words in stop_words]
+    print("Stopwords have been removed")
+
+
+def normalization(features):
+    stemmer = PorterStemmer()
+    lemma = WordNetLemmatizer()
+    for i in features:
+        i[1] = [stemmer.stem(word) for word in i[1]]
+    for i in features:
+        i[1] = [lemma.lemmatize(word=word, pos='v') for word in i[1]]
+    print("Text has been normalized")
+
+
+def remove_short_words(features):
+    for i in features:
+        i[1] = [word for word in i[1] if len(word) > 2]
+    print("Short words have been removed")
+
+
+# features_data, labels_data = load_csv('news.csv')
+
+# print(len(features_data))
+# print(features_data[0][0])
+
+# remove_url(features_data)
+# remove_newline(features_data)
+# remove_number(features_data)
+# remove_punctuation(features_data)
+# convert_into_lowercase(features_data)
+# tokenization(features_data)
+# remove_stopwords(features_data)
+# normalization(features_data)
+# remove_short_words(features_data)
+
+
+def processing(features):
+    remove_url(features)
+    remove_newline(features)
+    remove_number(features)
+    remove_punctuation(features)
+    convert_into_lowercase(features)
+    tokenization(features)
+    remove_stopwords(features)
+    # normalization(features)
+    remove_short_words(features)
+
+
+# (x_train, y_train), (x_test, y_test) = split_data(features_data, labels_data, 0.2)
+
+# for j in range(len(features_data)):
+#     print(j, features_data[j][1])
+
+
+
---
+++ Code/visualizeData.py
@@ -0,0 +1,44 @@
+import preprocessing
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def show_dataset(features, labels):
+    for i in range(len(features)):
+        print((i+1), features[i], labels[i])
+
+
+def split_features(features, labels):
+    features_real = []
+    features_fake = []
+    for i in range(len(features)):
+        if labels[i] == "REAL":
+            features_real.append(features[i][1])
+        else:
+            features_fake.append(features[i][1])
+    print("Real data number:" + str(len(features_real)) + "\nFake data number:" + str(len(features_fake)))
+    return features_real, features_fake
+
+
+def draw_plot(data):
+    temp = []
+    for i in data:
+        temp = temp + i
+    dict_word = {}
+    for i in temp:
+        dict_word[i] = dict_word.get(i, 0) + 1
+    dict_word = sorted(dict_word.items(), key=lambda x: x[1], reverse=True)[:11]
+    for i in dict_word:
+        plt.bar((i[0],), (i[-1],))
+    plt.show()
+
+
+features_data, labels_data = preprocessing.load_csv('news.csv')
+preprocessing.processing(features_data)
+# show_dataset(features_data, labels_data)
+features_real_data, features_fake_data = split_features(features_data, labels_data)
+# draw_plot(features_real_data)
+# draw_plot(features_fake_data)
+
+
+
